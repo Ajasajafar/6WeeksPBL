8e6417d9e13daa4577c0cf685f625ae2833344d4
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-11-08 17:56:03 +0100

    Initial commit

---
+++ week1/expense_tracker/main.py
@@ -0,0 +1,14 @@
+import csv
+from datetime import datetime
+
+def add_expenses(Category, Amount, Note=""):
+    with open('expenses.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([datetime.now().strftime(f"%d-%m-%Y")], Category, Amount, Note)
+
+def view_expenses():
+    with open('expenses.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+
