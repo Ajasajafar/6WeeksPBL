02d67545ab23ac6bc96fc266ef203ea4ccebf4b8
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-11-08 22:25:13 +0100

    Enabled Terminal Functionality

--- week1/expense_tracker/main.py
+++ week1/expense_tracker/main.py
@@ -1,17 +1,33 @@
 import csv
 from datetime import datetime
 
-def add_expenses(Category, Amount, Note=""):
-    with open('expenses.csv', 'a') as file:
-        writer = csv.writer(file)
-        # .writerow() takes only one argument
-        writer.writerow([datetime.now().strftime(f"%d-%m-%Y"), Category, Amount, Note])
 
-def view_expenses():
-    with open('expenses.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            print(row)
+while True:
+    print('''
+1. View all expenses
+2. Add an expense
+''')
+    reply = input()
+    def add_expenses(Category, Amount, Note=""):
+        with open('expenses.csv', 'a') as file:
+            writer = csv.writer(file)
+            # .writerow() takes only one argument
+            writer.writerow([datetime.now().strftime(f"%d-%m-%Y"), Category, Amount, Note])
 
-add_expenses('Food', 2000, "Hunger Problem Solved")
-view_expenses()
+    def view_expenses():
+        with open('expenses.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(row)
+                return row
+    if reply == '1':
+        view_expenses()
+    else:
+        a = input('Enter Category...')
+        b = input('Enter Amount...')
+        c = input('Enter description...')
+        add_expenses(a, b, c)
+        print('Expenses updated successfully!!')
+        view_expenses()
+
+
