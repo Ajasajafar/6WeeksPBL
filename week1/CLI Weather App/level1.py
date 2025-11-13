1ffa77d3cf0467f3b512770a9fea79dad2f2ed16
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-11-13 19:57:20 +0100

    Commit

--- week1/CLI Weather App/level1.py
+++ week1/CLI Weather App/level1.py
@@ -1,1 +1,19 @@
 import requests
+
+cityName = input('Enter city name')
+api_key = "755349663f5b82ad8bf6b90f0ed29a5c"
+url = f"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={api_key}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if data['cod'] == 200:
+    main = data["main"]
+    weather = data["weather"]
+    wind = data['wind']
+
+    print("\n weather information")
+    print(f"City: {cityName.title()}")
+    print(f"Temperature: {main['temp']}c")
+    print(f"weather: {weather['description'].title()}")
+    print(f"")
