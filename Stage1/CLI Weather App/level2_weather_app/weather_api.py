56854ade55b30190807bd0aa32dc5e5dcb0dd94e
Ajasajafar <bolarinwaayorinde97@gmail.com>
2025-11-17 05:41:12 +0100

    Handling API calls with weather_api.py

---
+++ Stage1/CLI Weather App/level2_weather_app/weather_api.py
@@ -0,0 +1,25 @@
+import requests
+from config import API_KEY 
+from level2 import base_url
+
+def get_weather_data(city):
+    '''Fetches weather data from OpenWeather API'''
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    try:
+        response = requests.get(base_url, params=params, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.Timeout:
+        print("⏳ Request timed out.")
+        return None
+    except requests.exceptions.ConnectionError:
+        print("🔌 No internet connection.")
+        return None
+    except requests.exceptions.HTTPError:
+        print("❗ Invalid API response or wrong city.")
+        return None 
